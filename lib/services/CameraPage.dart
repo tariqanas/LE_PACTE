c6c01e2e18eb783c88e20a538d9ede0d5e7aa02a
Anas <tariq.tariq.anas@gmail.com>
2022-07-12 13:53:59 +0200

    Add Camera feature

---
+++ lib/services/CameraPage.dart
@@ -0,0 +1,105 @@
+import 'package:camera/camera.dart';
+import 'package:eachday/homescreen.dart';
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription>? cameras;
+  const CameraPage({this.cameras, Key? key}) : super(key: key);
+
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController controller;
+  XFile? pictureFile;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(
+      widget.cameras![1],
+      ResolutionPreset.high,
+    );
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return const SizedBox(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              height:MediaQuery.of(context).size.height * 0.6,
+              child: CameraPreview(controller),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              pictureFile = await controller.takePicture();
+              setState(() {});
+            },
+            child: const Text('Envoies ta preuve ! 👹 '),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              pictureFile = await controller.takePicture();
+              setState(() {
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(title: 'Petite Nature 👎'),
+                  ),
+                );
+              });
+            },
+            child: const Text("Tu n'as pas les épaules.🐔"),
+          ),
+        ),
+   
+        if (pictureFile != null)
+             SizedBox(
+          child:
+          Wrap(
+            children: [
+            Image.network(
+            pictureFile!.path,
+            height: 200,
+          )
+            ],
+          )
+        )
+         
+        //Android/iOS
+        // Image.file(File(pictureFile!.path)))
+      ],
+    );
+  }
+}
